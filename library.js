63ffc9cb627ef6c24b5ee1394e1362d6f56486fa
Diego Vera <173311520+DiegoVT17@users.noreply.github.com>
2024-12-28 19:18:11 -0500

    add render and create book function

--- library.js
+++ library.js
@@ -1,4 +1,15 @@
-const myLibrary = [
+const bookForm = document.querySelector("form")
+const modal = document.querySelector("dialog")
+const btnModal = document.querySelector(".openModal")
+
+function Book(name, author, pages, relYear) {
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.releaseYear = relYear 
+}
+
+let myLibrary = [
   {
     name: "Lord of the Rings",
     author: "J.R.R. Tolkien",
@@ -13,38 +24,84 @@ const myLibrary = [
   }
 ]
 
+btnModal.addEventListener("click", () => {
+  modal.showModal()
+})
+
+function handleSubmit(e) {
+  e.preventDefault()
+  
+  const form = e.target;
+  const name = form.bookName.value
+  const author = form.author.value
+  const pages = form.pages.value
+  const relYear = form.release.value
+
+  addBookToLibrary(name, author, pages, relYear)
+  modal.close()
+  render(myLibrary)
+
+  form.bookName.value = ""
+  form.author.value = ""
+  form.pages.value = ""
+  form.release.value = ""
+}
+
+bookForm.addEventListener("submit", handleSubmit)
+
 function createBookTable(data) {
   const tRow = document.createElement("tr")
   const tName = document.createElement("td")
   const tAuthor = document.createElement("td")
   const tPages = document.createElement("td")
   const tRelYear = document.createElement("td")
+  const delBtn = document.createElement("button")
+  const tDel = document.createElement("td")
   
   tName.innerText = data.name
   tAuthor.innerText = data.author
   tPages.innerText = data.pages
   tRelYear.innerText = data.releaseYear
+  delBtn.textContent = "Borrar"
+  delBtn.addEventListener("click", () => {
+    delBook(data.name)
+  })
+  tDel.append(delBtn)
 
-  tRow.append(tName, tAuthor, tPages, tRelYear)
+  tRow.append(tName, tAuthor, tPages, tRelYear, tDel)
 
   return tRow
+}
+
+function delBook(name) {
+  myLibrary = myLibrary.filter(book => book.name !== name)
+  render()
 }
 
 function render() {
   const table = document.querySelector("table")
 
-  myLibrary.forEach(book => {
-    const newRow = createBookTable(book)
+  table.innerHTML = ""
 
+  const tRow = document.createElement("tr")
+  const tName = document.createElement("th")
+  const tAuthor = document.createElement("th")
+  const tPages = document.createElement("th")
+  const tRelYear = document.createElement("th")
+  const empty = document.createElement("th")
+
+  tName.innerText = "Name"
+  tAuthor.innerText = "Author"
+  tPages.innerText = "Pages"
+  tRelYear.innerText = "Release Year"
+
+  tRow.append(tName, tAuthor, tPages, tRelYear, empty)
+  table.append(tRow)
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const newRow = createBookTable(myLibrary[i])
     table.append(newRow)
-  })
-}
-
-function Book(name, author, pages, imgUrl) {
-  this.name = name
-  this.author = author
-  this.pages = pages
-  this.imgUrl = imgUrl
+  }
 }
 
 function addBookToLibrary(...args) {
@@ -52,4 +109,4 @@ function addBookToLibrary(...args) {
   myLibrary.push(newBook)
 }
 
-render()
+render(myLibrary)
